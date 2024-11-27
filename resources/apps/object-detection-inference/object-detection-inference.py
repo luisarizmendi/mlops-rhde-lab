22ff9377cfd673f7d79c2775c627e4c2d1b36d6c
Luis Arizmendi <luis.ariz@gmail.com>
2024-11-27 18:17:36 +0100

    debug false

--- resources/apps/object-detection-inference/object-detection-inference.py
+++ resources/apps/object-detection-inference/object-detection-inference.py
@@ -283,6 +283,6 @@ if __name__ == '__main__':
     initialize_camera()
     try:
         start_inference()
-        app.run(host='0.0.0.0', port=5000, debug=True)
+        app.run(host='0.0.0.0', port=5000, debug=False)
     finally:
         stop_inference()
