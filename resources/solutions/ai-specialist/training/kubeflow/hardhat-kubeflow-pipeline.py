c364b727394c3668b066e2b16e3b184ff1a051ae
Luis Arizmendi <luis.ariz@gmail.com>
2025-01-27 16:33:36 +0100

    pipeline to model registry

--- resources/solutions/ai-specialist/training/kubeflow/hardhat-kubeflow-pipeline.py
+++ resources/solutions/ai-specialist/training/kubeflow/hardhat-kubeflow-pipeline.py
@@ -17,30 +17,34 @@ def download_dataset(
     from roboflow import Roboflow
     import yaml
     import os
-    
+
     rf = Roboflow(api_key=api_key)
     project = rf.workspace(workspace).project(project)
     version = project.version(version)
     dataset = version.download("yolov11")
-    
+
     # Update data.yaml paths
     dataset_yaml_path = f"{dataset.location}/data.yaml"
     with open(dataset_yaml_path, "r") as file:
         data_config = yaml.safe_load(file)
-    
+
     data_config["train"] = f"{dataset.location}/train/images"
     data_config["val"] = f"{dataset.location}/valid/images"
     data_config["test"] = f"{dataset.location}/test/images"
-           
+
+
+    print(dataset)
+
+
+
     with open(dataset_path, "w") as f:
         f.write(dataset.location)
 
 
-    
 # Component 2: Train Model
 @dsl.component(
     base_image="quay.io/luisarizmendi/pytorch-custom:latest",
-    packages_to_install=["ultralytics", "torch"]
+    packages_to_install=["ultralytics", "torch", "pandas"]
 )
 def train_model(
     dataset_path: str,
@@ -50,15 +54,16 @@ def train_model(
     name: str = "yolo",
 ) -> NamedTuple('Outputs', [
     ('train_dir', str),
-    ('test_dir', str)
+    ('test_dir', str),
+    ('metrics', dict)
 ]):
     import torch
     from ultralytics import YOLO
-    from typing import NamedTuple
+    import pandas as pd
     import os
-    
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
+
     CONFIG = {
         'name': name,
         'model': 'yolo11m.pt',
@@ -68,10 +73,10 @@ def train_model(
         'imgsz': img_size,
         'device': device,
     }
-    
+
     # Configure PyTorch
     os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
-    
+
     # Initialize and train model
     model = YOLO(CONFIG['model'])
     results_train = model.train(
@@ -82,7 +87,7 @@ def train_model(
         imgsz=CONFIG['imgsz'],
         device=CONFIG['device'],
     )
-    
+
     # Evaluate model
     results_test = model.val(
         data=CONFIG['data'],
@@ -90,16 +95,30 @@ def train_model(
         device=CONFIG['device'],
         imgsz=CONFIG['imgsz']
     )
-    
-    # Export model
-    #model.export(format='onnx', imgsz=CONFIG['imgsz'])
-    #model.export(format="torchscript")       
-    
-    return NamedTuple('Outputs', [('train_dir', str), ('test_dir', str)])(
+
+    # Compute metrics from CSV
+    results_csv_path = os.path.join(results_train.save_dir, "results.csv")
+    results_df = pd.read_csv(results_csv_path)
+
+    # Extract metrics
+    metrics = {
+        "precision": results_df["metrics/precision(B)"].iloc[-1],
+        "recall": results_df["metrics/recall(B)"].iloc[-1],
+        "mAP50": results_df["metrics/mAP50(B)"].iloc[-1],
+        "mAP50-95": results_df["metrics/mAP50-95(B)"].iloc[-1]
+    }
+
+    return NamedTuple('Outputs', [
+        ('train_dir', str),
+        ('test_dir', str),
+        ('metrics', dict)
+    ])(
         train_dir=str(results_train.save_dir),
-        test_dir=str(results_test.save_dir)
+        test_dir=str(results_test.save_dir),
+        metrics=metrics
     )
 
+    
 # Component 3: Upload to MinIO
 @dsl.component(
     base_image="quay.io/luisarizmendi/pytorch-custom:latest",
@@ -113,27 +132,29 @@ def upload_to_minio(
     secret_key: str,
     bucket: str,
     model_path: dsl.OutputPath(str)
-) -> None:
+) -> NamedTuple('Outputs', [
+    ('model_artifact_s3_path', str)
+]):
     from minio import Minio
     from minio.error import S3Error
     import os
     import datetime
-    
+
     client = Minio(
         endpoint.replace('https://', '').replace('http://', ''),
         access_key=access_key,
         secret_key=secret_key,
         secure=True
     )
-    
+
     # Get paths for files
     weights_path = os.path.join(train_dir, "weights")
-    
-    files_train = [os.path.join(train_dir, f) for f in os.listdir(train_dir) 
+
+    files_train = [os.path.join(train_dir, f) for f in os.listdir(train_dir)
                    if os.path.isfile(os.path.join(train_dir, f))]
-    files_models = [os.path.join(weights_path, f) for f in os.listdir(weights_path) 
+    files_models = [os.path.join(weights_path, f) for f in os.listdir(weights_path)
                     if os.path.isfile(os.path.join(weights_path, f))]
-    
+
     files_model_pt = os.path.join(train_dir, "weights") + "/best.pt"
     
     #files_model_onnx = os.path.join(train_dir, "weights") + "/best.onnx"
@@ -176,133 +197,126 @@ def upload_to_minio(
     #    print(f"Error uploading {files_model_torchscript}: {e}")
 
 
+    model_artifact_s3_path=f"models/{directory_name}/model/pytorch/{os.path.basename(files_model_pt)}"
+
+    return NamedTuple('Outputs', [
+        ('model_artifact_s3_path', str)
+    ])(
+        model_artifact_s3_path
+    )
 
 
 
-
-
-
-
-
-
-
-
-# Component 4: Create Entry in Model Registry
+# Component 4: Push to Model Registry
 @dsl.component(
     base_image='python:3.9',
-    packages_to_install=[
-        'pip',  
-        'setuptools', 
-        'boto3',
-        'model-registry'
-    ]
+    packages_to_install=['model-registry']
 )
 def push_to_model_registry(
     model_name: str,
     version: str,
-    cluster_domain: str,
-    metrics: Input[Metrics],
-    dataset: Input[Dataset],
+    metrics: dict,
+    model_registry_name: str,
+    model_artifact_s3_path: str,
+    s3_endpoint: str,
+    roboflow_workspace: str,
+    roboflow_project: str,
+    roboflow_version: int,    
 ):
-
-    from os import environ, path, makedirs
-    from datetime import datetime
     from model_registry import ModelRegistry
-    import shutil
+    import os
+    from datetime import datetime
     import json
-    from boto3 import client
     
-    # Save to Model Registry
-    namespace_file_path =\
-        '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
-    with open(namespace_file_path, 'r') as namespace_file:
-        namespace = namespace_file.read()
-    
-    model_object_prefix = model_name if model_name else "model"
+    s3_endpoint_url=s3_endpoint.replace('https://', '').replace('http://', '')
     version = version if version else datetime.now().strftime('%y%m%d%H%M')
-        
+    model_object_prefix = model_name if model_name else "model"
+    cluster_domain= ""    
+    namespace_file_path = '/var/run/secrets/kubernetes.io/serviceaccount/namespace'
+    with open(namespace_file_path, 'r') as namespace_file:
+        namespace = namespace_file.read().strip()
+
+
+    # Get Cluster domain from MinIO s3_endpoint.
+    cluster_domain = s3_endpoint.split("//")[-1].split(".", 2)[-1]
+
+ 
+    os.environ["KF_PIPELINES_SA_TOKEN_PATH"] = "/var/run/secrets/kubernetes.io/serviceaccount/token"
+      
+   
     def _register_model(author_name , server_address, model_object_prefix, version):
         registry = ModelRegistry(server_address=server_address, port=443, author=author_name, is_secure=False)
         registered_model_name = model_object_prefix
         version_name = version
         metadata = {
-            "accuracy": str(metrics.metadata['Accuracy']),
-        } | dataset.metadata
-        
+            "dataset": f"https://universe.roboflow.com/{roboflow_workspace}/{roboflow_project}/dataset/{str(roboflow_version)}",
+            "mAP50": str(metrics["mAP50"]),
+            "mAP50-95": str(metrics["mAP50-95"]),
+            "precision": str(metrics["precision"]),
+            "recall": str(metrics["recall"])
+        }
+      
         rm = registry.register_model(
             registered_model_name,
-           "to-be-updated" if prod_flag else f"s3://{s3_endpoint_url.split('https://')[-1]}{model_artifact_s3_path}",
+            f"s3://{s3_endpoint_url}/{model_artifact_s3_path}",
             model_format_name="pt",
             model_format_version="1",
             version=version_name,
-            description=f"{registered_model_name} is a dense neural network used for Hardhat detection in images .",
+            description=f"{registered_model_name} is a dense neural network that detects Hardhats in images.",
             metadata=metadata
         )
         print("Model registered successfully")
-
+    
+    
     # Register the model
-    server_address = f"https://{namespace}-registry-rest.{cluster_domain}"
-    _register_model(namespace, server_address, model_object_prefix, version)
+    server_address = f"https://{model_registry_name}-rest.apps.{cluster_domain}"
+    print(f"s3://{s3_endpoint_url}/{model_artifact_s3_path}")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    _register_model(namespace, server_address, model_object_prefix, f"{model_object_prefix}-{version}")
+    
+    
 # Define the pipeline
 @dsl.pipeline(
     name='YOLO Training Pipeline',
     description='Pipeline to download data, train YOLO model, and upload results to MinIO'
 )
 def yolo_training_pipeline(
-    roboflow_api_key: str,
-    roboflow_workspace: str,
-    roboflow_project: str,
-    roboflow_version: int,
-    minio_endpoint: str,
-    minio_access_key: str,
-    minio_secret_key: str,
-    minio_bucket: str,
-    cluster_domain: str,
-    train_name: str = "hardhat",
-    train_epochs: int = 50,
-    train_batch_size: int = 16,
-    train_img_size: int = 640,
+
+    #roboflow_api_key: str,
+    #roboflow_workspace: str,
+    #roboflow_project: str,
+    #roboflow_version: int,
+    #minio_endpoint: str,
+    #minio_access_key: str,
+    #minio_secret_key: str,
+    #minio_bucket: str,
+    roboflow_api_key: str = "kZAFZ30ULoBdNjSmyxlK",
+    roboflow_workspace: str = "workshop-igjqz",
+    roboflow_project: str = "group-99-hardhat-detection",
+    roboflow_version: int = 4,
+    minio_endpoint: str = "https://minio-api-minio.apps.cluster-q4bsk.q4bsk.sandbox2707.opentlc.com",
+    minio_access_key: str = "user99",
+    minio_secret_key: str = "redhat99",
+    minio_bucket: str = "user99-ai-models",
     pvc_storage_class: str = "gp3-csi",
     pvc_size: str = "5Gi",
-    pvc_name_sufix: str = "-kubeflow-pvc",
+    pvc_name_suffix: str = "-kubeflow-pvc",
+    train_name: str = "user99-hardhat",
+    #train_epochs: int = 50,
+    #train_batch_size: int = 16,
+    train_epochs: int = 1,
+    train_batch_size: int = 1,
+    train_img_size: int = 640,
+    model_registry_name: str = "object-detection-model-registry",
 ):
     # Create PV
     pvc = kubernetes.CreatePVC(
-        pvc_name_suffix=pvc_name_sufix,
+        pvc_name_suffix=pvc_name_suffix,
         access_modes=['ReadWriteOnce'],
         size=pvc_size,
         storage_class_name=pvc_storage_class,
     )
-        
+
     # Download dataset
     download_task = download_dataset(
         api_key=roboflow_api_key,
@@ -316,7 +330,7 @@ def yolo_training_pipeline(
         pvc_name=pvc.outputs['name'],
         mount_path='/opt/app-root/src',
     )
-    
+
     # Train model
     train_task = train_model(
         dataset_path=download_task.output,
@@ -331,7 +345,7 @@ def yolo_training_pipeline(
         pvc_name=pvc.outputs['name'],
         mount_path='/opt/app-root/src',
     )
-    
+
     # Upload results
     upload_task = upload_to_minio(
         train_dir=train_task.outputs['train_dir'],
@@ -347,20 +361,23 @@ def yolo_training_pipeline(
         pvc_name=pvc.outputs['name'],
         mount_path='/opt/app-root/src',
     )
+    
+    # Push to model registry
+    push_to_model_registry(
+        model_name=train_name,
+        version="",
+        metrics=train_task.outputs['metrics'],
+        model_registry_name=model_registry_name,
+        model_artifact_s3_path=upload_task.outputs['model_artifact_s3_path'],
+        s3_endpoint=minio_endpoint,
+        roboflow_workspace=roboflow_workspace,
+        roboflow_project=roboflow_project,
+        roboflow_version=roboflow_version
+    ).after(upload_task)
 
     delete_pvc = kubernetes.DeletePVC(
         pvc_name=pvc.outputs['name']
     ).after(upload_task)
-
-    # Create entry in Model Registry
-    push_to_model_registry_task = push_to_model_registry(
-        model_name=train_name,
-        version="",
-        cluster_domain=cluster_domain,
-        metrics=train_task.outputs['metrics'],
-        dataset=download_task.outputs['dataset']
-    ).after(upload_task)
-
 
 if __name__ == "__main__":
     # Compile the pipeline
@@ -369,11 +386,5 @@ if __name__ == "__main__":
         package_path='yolo_training_pipeline.yaml'
     )
     print("Pipeline compiled successfully to yolo_training_pipeline.yaml")
-    
-    
-    
-    
-    
-    
-    
-    
+
+
